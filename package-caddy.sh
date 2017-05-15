001ef10b60aa6d5809cb9a64a5a9194db9b60663
David Anderson <dave@natulte.net>
2017-05-14 20:39:54 -0700

    Use the right keys for packagecloud, derp.

--- package-caddy.sh
+++ package-caddy.sh
@@ -2,7 +2,6 @@
 
 set -euxo pipefail
 
-# foo
 wget -O- https://caddyserver.com/download/linux/amd64?plugins=http.prometheus,http.proxyprotocol | tar -zxvf- caddy
 fpm -s dir -t deb -n caddy -v $(date '+%Y%m%d%H%M%S') ./caddy=/usr/bin/caddy
 rm caddy
