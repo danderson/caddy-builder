6bf25633ffdd53a717f41d2e191aa7e548b7967d
David Anderson <dave@natulte.net>
2017-05-21 02:03:47 -0700

    Some fixes as the plan makes contact with the enemy.

--- package-caddy.sh
+++ package-caddy.sh
@@ -3,5 +3,11 @@
 set -euxo pipefail
 
 wget -O- https://caddyserver.com/download/linux/amd64?plugins=http.prometheus,http.proxyprotocol | tar -zxvf- caddy
-fpm -s dir -t deb -n caddy -v $(date '+%Y%m%d%H%M%S') --deb-systemd=./caddy.service --deb-systemd-restart-after-upgrade --after-install=./create-caddy-user.sh  ./caddy=/usr/bin/caddy 
+fpm -s dir -t deb -n caddy -v $(date '+%Y%m%d%H%M%S') \
+    --directories=/etc/caddy/sites.d \
+    --directories=/etc/ssl/caddy \
+    --deb-systemd=./caddy.service \
+    --deb-systemd-restart-after-upgrade \
+    --after-install=./create-caddy-user.sh \
+    ./caddy=/usr/bin/caddy 
 rm caddy
